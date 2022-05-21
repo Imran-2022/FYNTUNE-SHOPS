a97bd3ba380da661d996906438518b8434b57762
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-21 23:29:18 +0600

    filter by area'

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Home = () => {
@@ -7,18 +7,53 @@ const Home = () => {
     const storeLength = useSelector((state) => {
         return state.stores;
     })
-    
+    const [users, setUsers] = useState(storeLength);
+    const [filterblood,setFilterBlood]=useState(users)
+    const [group,setGroup]=useState("ALL")
+    const filterImage=(fimage)=>{
+       
+        setFilterBlood(users)
+        console.log("fimage",fimage);
+        setGroup(fimage)
+        if(fimage==="ALL")
+        {
+            setFilterBlood(users)
+        }
+        else{
+            const filterImages = users.filter((x)=>{
+                console.log("x",x)
+                return x.shop_area===fimage;
+        })
+        setFilterBlood(filterImages)
+
+        }
+    }
     return (
         <>
+
             <p>fyntune-shop</p>
             <p>length of the stores {storeLength.length}</p>
+            <div className="catagories py-5 ms-5">
+                <label for="blood">Choose a Area : &nbsp;</label>
+                <select id="blood" name="bloodlist" onChange={(e)=>filterImage(e.target.value)}>
+                    <option selected="selected" value="ALL">ALL</option>
+                    <option value="Thane">Thane</option>
+                    <option value="Pune">Pune</option>
+                    <option value="Mumbai-Suburban">Mumbai-Suburban</option>
+                    <option value="Nashik">Nashik</option>
+                    <option value="Nagpur">Nagpur</option>
+                    <option value="Ahmednagar">Ahmednagar</option>
+                    <option value="Solapur">Solapur</option>
+                </select>
+                &nbsp; available donar for {group} : {filterblood.length}
+            </div>
             <div style={{display: 'flex',gap:"30px",margin:"50px",flexWrap:"wrap", }}>
                 {
-                    storeLength.map((dt, idx) => {
+                    filterblood.map((dt, idx) => {
                         const { shop_name, shop_id, shop_area, shop_category, open, close } = dt
                         console.log(shop_name, shop_id, shop_area, shop_category, open, close)
                         return (
-                            <div key={idx}  style={{padding:"10px",boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",minWidth:"200px"}}>
+                            <div key={idx}  style={{padding:"10px",boxShadow:"rgba(0, 0, 0, 0.16) 0px 1px 4px",minWidth:"200px"}}>
 
                                 <p>{shop_name}</p>
                                 <p>{shop_id}</p>
--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -10,7 +10,7 @@ const initialState = [
       {
         shop_name: 'shop shop2',
         shop_id: '2',
-        shop_area: 'Thane',
+        shop_area: 'Pune',
         shop_category: 'Grocery',
         open: '2022-05-26',
         close: '2022-05-10'
@@ -18,7 +18,7 @@ const initialState = [
     {
         shop_name: 'shop shop3',
         shop_id: '3',
-        shop_area: 'Thane',
+        shop_area: 'Mumbai-Suburban',
         shop_category: 'Grocery',
         open: '2022-06-03',
         close: '2022-06-01'
@@ -26,7 +26,7 @@ const initialState = [
       {
         shop_name: 'shop shop4',
         shop_id: '4',
-        shop_area: 'Thane',
+        shop_area: 'Nashik',
         shop_category: 'Grocery',
         open: '2022-05-26',
         close: '2022-05-10'
@@ -34,7 +34,7 @@ const initialState = [
     {
         shop_name: 'shop shop5',
         shop_id: '5',
-        shop_area: 'Thane',
+        shop_area: 'Nagpur',
         shop_category: 'Grocery',
         open: '2022-06-03',
         close: '2022-06-01'
@@ -42,7 +42,7 @@ const initialState = [
       {
         shop_name: 'shop shop6',
         shop_id: '6',
-        shop_area: 'Thane',
+        shop_area: 'Ahmednagar',
         shop_category: 'Grocery',
         open: '2022-05-26',
         close: '2022-05-10'
@@ -50,7 +50,7 @@ const initialState = [
     {
         shop_name: 'shop shop7',
         shop_id: '7',
-        shop_area: 'Thane',
+        shop_area: 'Solapur',
         shop_category: 'Grocery',
         open: '2022-06-03',
         close: '2022-06-01'
