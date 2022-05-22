a202abb495ac5e827b394eca1f70ff1606453381
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-22 12:41:16 +0600

    crud done:

--- src/components/addShops/UpdateModal.js
+++ src/components/addShops/UpdateModal.js
@@ -14,16 +14,20 @@ const UpdateModal = ({ setShowModal, showModal,shop_id }) => {
         reset()
     };
     // dispatch(shopsU(d));
-
+    const storeLength = useSelector((state) => {
+        return state.stores.filter(dt=>dt.shop_id===shop_id);
+    })
+    console.log("object",storeLength[0]);
     return (
         <div className="Component">
             <form className="form" onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", gap: "20px" }}>
-                <input placeholder="Shop Name" {...register("shop_name", { required: true })} autoComplete="off" />
+                <input defaultValue={storeLength[0]?.shop_name} placeholder="Shop Name" {...register("shop_name", { required: true })} autoComplete="off" />
                 {errors.shop_name && <p>This field is required</p>}
                 <input type="hidden" defaultValue={shop_id} {...register("shop_id")} autoComplete="off" readOnly />
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <p>SHOPS AREA</p>
                     <select {...register("shop_area", { required: true })} className="p-1 mb-3">
+                    <option value={storeLength[0]?.shop_area} selected disabled hidden>{storeLength[0]?.shop_area}</option>
                         <option value="Thane">Thane</option>
                         <option value="Pune">Pune</option>
                         <option value="Mumbai-Suburban">Mumbai-Suburban</option>
@@ -37,6 +41,7 @@ const UpdateModal = ({ setShowModal, showModal,shop_id }) => {
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <p>SHOPS CATEGORY -</p>
                     <select {...register("shop_category", { required: true })} className="p-1 mb-3">
+                    <option value={storeLength[0]?.shop_category} selected disabled hidden>{storeLength[0]?.shop_category}</option>
                         <option value="Grocery">Grocery</option>
                         <option value="Butcher">Butcher</option>
                         <option value="Baker">Baker</option>
@@ -50,19 +55,21 @@ const UpdateModal = ({ setShowModal, showModal,shop_id }) => {
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <div>
                         <p style={{ marginBottom: "10px" }}>Open Date</p>
-                        <input type="date" {...register("open", { required: true })} />
+                        <input defaultValue={storeLength[0]?.open}  type="date" {...register("open", { required: true })} />
                         {errors.open && <p>This field is required</p>}
                     </div>
                     <div>
                         <p style={{ marginBottom: "10px" }}>closing date</p>
-                        <input type="date"  {...register("close", { required: true })} />
+                        <input defaultValue={storeLength[0]?.close}  type="date"  {...register("close", { required: true })} />
                         {errors.close && <p>This field is required</p>}
                     </div>
                 </div>
 
                 <input className="my-3" type="submit"  value="Update SHOPS" />
+                <input className="my-3" type="submit"onClick={() => setShowModal(!showModal)}  value="close" />
+                {/* <button onClick={() => setShowModal(!showModal)}>close</button> */}
             </form>
-            <button onClick={() => setShowModal(!showModal)}>close</button>
+            
         </div>
     );
 };
